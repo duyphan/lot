300e9def0cbb483eaf81f7b1219eca816cd9c60e
Phan  Duy <duy.phan@asnet.com.vn>
2015-02-10 11:09:54 +0700

    change folder chef

--- chef/site-cookbooks/basic/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'basic'
-maintainer       'AsNet Co., Ltd'
-maintainer_email 'YOUR_EMAIL'
-license          'asoft@asnet.com.vn'
-description      'Installs/Configures basic'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
-depends "postgresql"
